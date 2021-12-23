88fc32d1bf44d46701f8d2116c331b1abe64f257
Jesse Brink <jbrink0925@gmail.com>
2021-12-22 20:47:11 -0500

    add mock vehicle data and change data type for id to SERIAL.

---
+++ vehicles.sql
@@ -0,0 +1,27 @@
+create table vehicles (
+	id SERIAL,
+	make VARCHAR(50),
+	model VARCHAR(50),
+	year VARCHAR(50),
+	price VARCHAR(50)
+);
+insert into vehicles (make, model, year, price) values ('Kia', 'Sportage', 1997, '$12600.58');
+insert into vehicles (make, model, year, price) values ('Mercury', 'Mystique', 1997, '$35193.18');
+insert into vehicles (make, model, year, price) values ('Oldsmobile', 'Cutlass', 1998, '$9263.35');
+insert into vehicles (make, model, year, price) values ('Mitsubishi', 'Montero', 1997, '$29874.00');
+insert into vehicles (make, model, year, price) values ('Infiniti', 'QX', 2012, '$11202.23');
+insert into vehicles (make, model, year, price) values ('Lexus', 'IS', 2011, '$27467.98');
+insert into vehicles (make, model, year, price) values ('Jeep', 'Commander', 2008, '$17653.17');
+insert into vehicles (make, model, year, price) values ('Volvo', 'V70', 2009, '$31409.74');
+insert into vehicles (make, model, year, price) values ('Dodge', 'Caravan', 1996, '$16873.17');
+insert into vehicles (make, model, year, price) values ('Acura', 'RL', 2006, '$16789.23');
+insert into vehicles (make, model, year, price) values ('Pontiac', 'Grand Prix', 1975, '$13770.68');
+insert into vehicles (make, model, year, price) values ('Buick', 'Electra', 1987, '$20393.16');
+insert into vehicles (make, model, year, price) values ('Toyota', 'Celica', 1994, '$14262.48');
+insert into vehicles (make, model, year, price) values ('Saturn', 'Relay', 2006, '$25058.27');
+insert into vehicles (make, model, year, price) values ('Chevrolet', 'Silverado', 2006, '$37628.13');
+insert into vehicles (make, model, year, price) values ('Subaru', 'XT', 1989, '$39227.24');
+insert into vehicles (make, model, year, price) values ('Lexus', 'ES', 2006, '$32468.58');
+insert into vehicles (make, model, year, price) values ('Porsche', 'Cayenne', 2003, '$37238.92');
+insert into vehicles (make, model, year, price) values ('Maserati', 'Spyder', 2002, '$29732.44');
+insert into vehicles (make, model, year, price) values ('Chevrolet', 'Corvette', 1969, '$22255.67');
